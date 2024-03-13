f8b82c11597eaab55d94e878f1ba18ae8bd1ed62
Sergei Morozov <moroz051091@yandex.ru>
2024-03-13 19:18:03 +0400

    feat(views): add task-specific views

--- data_processor/views.py
+++ data_processor/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from data_processor.serializers import Payload
+from data_processor.models import Instrument
+
+
+class IsinExistsView(APIView):
+    serializer_class = Payload
+
+    def get(self, request):
+        date = request.query_params.get('date')
+        instrument = request.query_params.get('instrument')
+        exchange = request.query_params.get('exchange')
+        queryset = Instrument.objects.all()
+
+        if date:
+            queryset = queryset.filter(exchange__date__date=date)
+        if instrument:
+            queryset = queryset.filter(name=instrument)
+        if exchange:
+            queryset = queryset.filter(exchange__name=exchange)
+
+        serializer = Payload(queryset, many=True)
+        return Response({'result': serializer.data})
+
+
+class IsinExistsIntervalView(APIView):
+    def get(self, request):
+        date = request.query_params.get('date')
+        instrument = request.query_params.get('instrument')
+        exchange = request.query_params.get('exchange')
+        queryset = Instrument.objects.filter(
+            exchange__date__date=date,
+            name=instrument,
+            exchange__name=exchange
+        ).first()
+
+        serializer = Payload(queryset)
+        return Response({'result': serializer.data if queryset else None})
+
+
+class IidToIsinView(APIView):
+    def get(self, request):
+        date = request.query_params.get('date')
+        iid = int(request.query_params.get('iid'))
+        queryset = Instrument.objects.filter(
+            iid=iid,
+            exchange__date__date=date
+        ).first()
+
+        serializer = Payload(queryset)
+        return Response({'result': serializer.data if queryset else None})
