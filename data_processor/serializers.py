5d09b3b2ca016e63b2e72aadcd27dda33e16f1aa
Sergei Morozov <moroz051091@yandex.ru>
2024-03-15 05:35:24 +0400

    feat(views): integrate input data validation and refactor business logic

--- data_processor/serializers.py
+++ data_processor/serializers.py
@@ -9,3 +9,21 @@ class Payload(serializers.ModelSerializer):
     class Meta:
         model = Instrument
         fields = ['name', 'exchange', 'iid', 'storage_type']
+
+
+class IsinExistsFilterSerializer(serializers.Serializer):
+    date = serializers.DateField(required=False)
+    instrument = serializers.CharField(max_length=100, required=False)
+    exchange = serializers.CharField(max_length=100, required=False)
+
+
+class IsinExistsIntervalFilterSerializer(serializers.Serializer):
+    date_from = serializers.DateField(required=False)
+    date_to = serializers.DateField(required=False)
+    instrument = serializers.CharField(max_length=100, required=False)
+    exchange = serializers.CharField(max_length=100, required=False)
+
+
+class IidToIsinFilterSerializer(serializers.Serializer):
+    date = serializers.DateField(required=False)
+    iid = serializers.IntegerField(required=False)
