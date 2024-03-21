c04e09e91ac4dfcaf42312f2ed880ee7419b98da
Sergei Morozov <moroz051091@yandex.ru>
2024-03-22 01:56:35 +0400

    feat(router): Add router for streaming binary files

--- router.py
+++ router.py
@@ -1,18 +1,21 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
 from services import search_data
+from starlette.responses import StreamingResponse
+from file_streaming import configure_stream_response
 from serializers import (Payload,
                          IsinExistsFilterSerializer,
                          IsinExistsIntervalFilterSerializer,
                          IidToIsinFilterSerializer,
+                         StreamSerializer,
                          )
 
-router = APIRouter(
+router_search = APIRouter(
     prefix="/api"
 )
 
 
-@router.get("/isin_exists")
+@router_search.get("/isin_exists")
 async def isin_exists(
         attr: Annotated[IsinExistsFilterSerializer, Depends()]
 ) -> list[Payload]:
@@ -20,7 +23,7 @@ async def isin_exists(
     return await search_data(s_attr)
 
 
-@router.get("/isin_exists_interval")
+@router_search.get("/isin_exists_interval")
 async def isin_exists_interval(
         attr: Annotated[IsinExistsIntervalFilterSerializer, Depends()]
 ) -> list[Payload]:
@@ -28,9 +31,20 @@ async def isin_exists_interval(
     return await search_data(s_attr)
 
 
-@router.get("/iid_to_isin")
+@router_search.get("/iid_to_isin")
 async def iid_to_isin(
         attr: Annotated[IidToIsinFilterSerializer, Depends()]
 ) -> list[Payload]:
     s_attr = attr.model_dump()
     return await search_data(s_attr)
+
+
+router_stream = APIRouter()
+
+
+@router_stream.get("/stream")
+async def stream_binary_file(
+        attr: Annotated[StreamSerializer, Depends()]
+):
+    s_attr = attr.model_dump()
+    return StreamingResponse(**await configure_stream_response(s_attr))
