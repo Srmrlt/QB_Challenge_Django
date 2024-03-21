42a22a3bc58ebd66c004cd73aa45463d619c6127
Sergei Morozov <moroz051091@yandex.ru>
2024-03-21 13:22:57 +0400

    feat(model queries): Add find_data method with search_conditions

--- database/queries.py
+++ database/queries.py
@@ -1,6 +1,7 @@
-from sqlalchemy import text, insert, select
+from sqlalchemy import text, insert, select, and_
+from sqlalchemy.orm import joinedload, load_only
 from database.database import engine, session_factory, Base
-from database.models import DateOrm, ExchangeOrm
+from database.models import DateOrm, ExchangeOrm, InstrumentOrm
 
 
 class OrmMethods:
@@ -27,3 +28,21 @@ class OrmMethods:
                     session.add(new_data)
                     await session.flush()  # Fix changes to get an id of a new data
                     return new_data.id
+
+    @staticmethod
+    async def find_data(search_conditions: list):
+        async with session_factory() as session:
+            instrument_load_list = [InstrumentOrm.name, InstrumentOrm.iid, InstrumentOrm.storage_type]
+            query = (select(InstrumentOrm)
+                     .join(InstrumentOrm.exchange)
+                     .join(ExchangeOrm.date)
+                     .options(joinedload(InstrumentOrm.exchange)
+                              .load_only(ExchangeOrm.name)
+                              )
+                     .options(load_only(*instrument_load_list))
+                     .filter(and_(*search_conditions))
+                     )
+
+            result = await session.execute(query)
+            data = result.scalars().all()
+            return data
