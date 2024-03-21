9a102374dd06469154261e9a72379dbb15331f26
Sergei Morozov <moroz051091@yandex.ru>
2024-03-22 01:58:44 +0400

    feat(serializers): Configure serializers, add StreamSerializer

--- serializers.py
+++ serializers.py
@@ -1,27 +1,34 @@
 import datetime
-from pydantic import BaseModel
+from typing import Optional
+from pydantic import BaseModel, conint
 
 
 class Payload(BaseModel):
     instrument: str
     exchange: str
     iid: int
-    storage_type: int
+    storage_type: str
     
 
 class IsinExistsFilterSerializer(BaseModel):
-    date: datetime.date
-    instrument: str
-    exchange: str
+    date: Optional[datetime.date] = None
+    instrument: Optional[str] = None
+    exchange: Optional[str] = None
 
 
 class IsinExistsIntervalFilterSerializer(BaseModel):
-    date_from: datetime.date
-    date_to: datetime.date
-    instrument: str
-    exchange: str
+    date_from: Optional[datetime.date] = None
+    date_to: Optional[datetime.date] = None
+    instrument: Optional[str] = None
+    exchange: Optional[str] = None
 
 
 class IidToIsinFilterSerializer(BaseModel):
+    date: Optional[datetime.date] = None
+    iid: Optional[int] = None
+
+
+class StreamSerializer(BaseModel):
     date: datetime.date
-    iid: int
+    filename: str
+    chunk: conint(gt=4*1024, le=512*1024) = 32*1024
