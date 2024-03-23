a67309a7202e62c608ff56f02a2a9f18ff94f997
Sergei Morozov <moroz051091@yandex.ru>
2024-03-23 20:54:34 +0400

    refactor(services): Enhance code readability and refine naming

--- services/file_streaming.py
+++ services/file_streaming.py
@@ -1,30 +1,26 @@
 import os
+import datetime
 import aiofiles
 from typing import Any, AsyncGenerator
 from fastapi import HTTPException
 
 
-async def configure_stream_response(validated_data: dict[str, Any]) -> dict[str, Any]:
+async def configure_stream_response(request_data: dict[str, Any]) -> dict[str, Any]:
     """
     Prepares parameters for a StreamingResponse based on validated data.
-
-    Args:
-        validated_data (dict[str, Any]): Dictionary of validated data needed for setting up the response.
-
-    Returns:
-        dict[str, Any]: Dictionary with 'content' and 'headers' keys, ready to be used in a StreamingResponse.
+    :param request_data: Dictionary of validated data needed for setting up the response.
+    :return: Dictionary with 'content' and 'headers' keys, ready to be used in a StreamingResponse.
     """
-    chunk_size = validated_data.get('chunk')
-    date = validated_data.get('date')
-    filename = validated_data.get('filename')
-    file_path = os.path.join('../data', str(date.year), str(date.month), str(date.day), filename)
-    await __check_file_availability(file_path)
+    chunk_size = request_data.get('chunk')
+    date_as_path = request_data.get('date')
+    filename = request_data.get('filename')
+    file_path = _create_file_path(date_as_path, filename)
     file_size = os.path.getsize(file_path)
     headers = {
         'Content-Length': str(file_size),
         'Content-Disposition': f'attachment; filename="{os.path.basename(file_path)}"',
     }
-    content = __read_file_in_chunks(file_path, chunk_size)
+    content = _read_file_in_chunks(file_path, chunk_size)
 
     return {
         'content': content,
@@ -32,15 +28,22 @@ async def configure_stream_response(validated_data: dict[str, Any]) -> dict[str,
     }
 
 
-async def __check_file_availability(file_path: str):
+def _create_file_path(date_as_path: datetime.date, filename: str) -> str:
     """
-    Asynchronously checks if file exists, raises error if not.
+    Asynchronously create file path and check if file exists, raise error if not.
     """
+    file_path = os.path.join('data',
+                             str(date_as_path.year),
+                             str(date_as_path.month),
+                             str(date_as_path.day),
+                             filename
+                             )
     if not os.path.exists(file_path):
         raise HTTPException(status_code=404, detail="File not found")
+    return file_path
 
 
-async def __read_file_in_chunks(file_path: str, chunk_size: int) -> AsyncGenerator[bytes]:
+async def _read_file_in_chunks(file_path: str, chunk_size: int) -> AsyncGenerator[bytes, Any]:
     """
     Asynchronously read a file in chunks of a specified size.
 
--- services/xml_parser.py
+++ services/xml_parser.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any
 from lxml import etree
 from inflection import underscore as camel_to_snake
 from sqlalchemy.exc import SQLAlchemyError
@@ -9,7 +10,7 @@ from database.models import *
 async def parse_data():
     try:
         # Parsing the xml files in data directory
-        await _parse_directory('../data')
+        await _parse_directory('data')
     except Exception as e:
         raise Exception(f'Error parsing data: {e}')
 
@@ -17,7 +18,6 @@ async def parse_data():
 async def _parse_directory(root_dir: str):
     """
     Walk through the directories and parse 'manifest.xml' in each subdirectory.
-
     :param root_dir: The root directory from which to start the walk.
     """
     for subdir, dirs, files in os.walk(root_dir):
@@ -36,43 +36,92 @@ async def _parse_manifest(xml_path: str):
     """
     Parse the XML manifest file,
     create or update database records for the date, exchanges, and instruments.
-
     :param xml_path: The file path of the manifest XML.
     """
-    try:
-        tree = etree.parse(xml_path)
-        root = tree.getroot()
-    except etree.XMLSyntaxError:
-        raise
+    root = _parse_xml_file(xml_path)
 
-    try:
-        date = root.find('Date').text
-        date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-        date_pk = await OrmMethods.add_new_data(DateOrm, {'date': date})
-    except SQLAlchemyError as e:
-        raise Exception(f'Database error creating ManifestDate for: {e}')
+    # Read Date from file
+    date_pk = await _process_manifest_date(root)
 
+    #  Read Exchange params from file
     for exchange in root.xpath('.//Exchange'):
-        try:
-            attributes = {camel_to_snake(attr): exchange.get(attr)
-                          for attr in ['Name', 'Location']}
-            attributes['date_id'] = date_pk
-            exchange_pk = await OrmMethods.add_new_data(ExchangeOrm, attributes)
-        except SQLAlchemyError as e:
-            print(f'Database error processing exchange: {e}')
-            continue
+        exchange_pk = await _process_exchange(exchange, date_pk)
 
+        #  Read Instrument params from file
         for instrument in exchange.xpath('.//Instrument'):
-            try:
-                attributes = {camel_to_snake(attr): instrument.get(attr)
-                              for attr in ['Name', 'StorageType', 'Levels', 'Iid',
-                                           'AvailableIntervalBegin', 'AvailableIntervalEnd']}
-                attributes['iid'] = int(attributes['iid'])
-                attributes['available_interval_begin'] = datetime.datetime.strptime(attributes['available_interval_begin'], '%H:%M').time()
-                attributes['available_interval_end'] = datetime.datetime.strptime(attributes['available_interval_end'],
-                                                                                  '%H:%M').time()
-                attributes['exchange_id'] = exchange_pk
-                await OrmMethods.add_new_data(InstrumentOrm, attributes)
-            except SQLAlchemyError as e:
-                print(f'Database error processing instrument: {e}')
-                continue
+            await _process_instrument(instrument, exchange_pk)
+
+
+def _parse_xml_file(path: str):
+    """
+    Parses an XML file and returns its root element.
+    :param path: The file system path to the XML file.
+    :return: An Element object which is the root of the XML tree.
+    """
+    tree = etree.parse(path)
+    return tree.getroot()
+
+
+def _sqlalchemy_exception_handler(func):
+    """
+    Decorator to handle SQLAlchemy exceptions for asynchronous functions.
+    """
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except SQLAlchemyError as e:
+            raise Exception(f'Database error in {func.__name__}: {e}')
+    return wrapper
+
+
+@_sqlalchemy_exception_handler
+async def _process_manifest_date(root):
+    """
+    Asynchronously processes and stores date data in the database.
+    :param root: An Element object which is the root of the XML tree.
+    """
+    date = root.find('Date').text
+    date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+    return await OrmMethods.add_new_data(DateOrm, {'date': date})
+
+
+@_sqlalchemy_exception_handler
+async def _process_exchange(exchange, date_pk):
+    """
+    Asynchronously processes and stores exchange data in the database with associated date primary key.
+    :param exchange: The object representing the exchange.
+    :param date_pk: The primary key (integer) of the date to which this exchange belongs.
+    """
+    attributes = _get_attributes(exchange, ['Name', 'Location'])
+    attributes['date_id'] = date_pk
+    return await OrmMethods.add_new_data(ExchangeOrm, attributes)
+
+
+@_sqlalchemy_exception_handler
+async def _process_instrument(instrument, exchange_pk):
+    """
+    Asynchronously processes and stores instrument data in the database with associated exchange primary key.
+    :param instrument: The object representing the instrument.
+    :param exchange_pk: The primary key (integer) of the exchange to which this instrument belongs.
+    """
+    attributes: dict[str, Any] = _get_attributes(
+        instrument,
+        ['Name', 'StorageType', 'Levels', 'Iid', 'AvailableIntervalBegin', 'AvailableIntervalEnd']
+    )
+    attributes['iid'] = int(attributes['iid'])
+    attributes['available_interval_begin'] = (
+        datetime.datetime.strptime(attributes['available_interval_begin'], '%H:%M').time())
+    attributes['available_interval_end'] = (
+        datetime.datetime.strptime(attributes['available_interval_end'], '%H:%M').time())
+    attributes['exchange_id'] = exchange_pk
+    await OrmMethods.add_new_data(InstrumentOrm, attributes)
+
+
+def _get_attributes(element, attrs: list[str]) -> dict[str, str]:
+    """
+    Extracts specified attributes from an element and returns them as a dictionary with snake_case keys.
+    :param element: The object or element from which attributes are to be extracted.
+    :param attrs: A list of attribute names (strings) that are to be extracted from the element.
+    :return: A dictionary where keys are the snake_case versions of the attribute names provided in 'attrs'.
+    """
+    return {camel_to_snake(attr): element.get(attr) for attr in attrs}
